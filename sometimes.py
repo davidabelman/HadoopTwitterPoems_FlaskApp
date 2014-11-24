ecd87d2da6990f69dc35aae1d88ffdca940fa74e
David <davidabelman@gmail.com>
2014-11-24 21:16:41 +0700

    Update

--- sometimes.py
+++ sometimes.py
@@ -1,5 +1,5 @@
 title = "Sometimes (VII)"
-desc = "Data from November 14-22, 2014 (filtered for 7 word lines)"
+desc = "Data from November 14-22, 2014 (filtered for 7 word lines) "
 poem = """
 sometimes i just want to know why	qaiyim1500	November 17, 2014	https://twitter.com/qaiyim1500/status/534436900392935424
 sometimes i just want to fucking cry.	ty_vaughn	November 20, 2014	https://twitter.com/ty_vaughn/status/535389012564144129
